e706c6db0a7de2ae021d799d017d218ac6281e34
Miguel angel Munoz fernandez <mmunoz-f@c1r4s5.42madrid.com>
2021-03-15 13:57:42 +0100

    15/03/2021 13:57:42 - lstnew bug fixed

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: mmunoz-f <mmunoz-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/25 16:13:46 by marvin            #+#    #+#             */
-/*   Updated: 2021/03/14 11:57:26 by mmunoz-f         ###   ########.fr       */
+/*   Updated: 2021/03/15 13:56:34 by mmunoz-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,9 @@ t_list	*ft_lstnew(void *content)
 {
 	t_list	*lst;
 
-	lst = (t_list *)malloc(sizeof(lst));
+	lst = (t_list *)ft_calloc(1, sizeof(t_list));
 	if (!lst)
 		return (0);
 	lst->content = content;
-	lst->next = 0;
-	lst->previous = 0;
 	return (lst);
 }
