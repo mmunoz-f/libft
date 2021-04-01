5bc6d3e0d665cc5a22a77f2acc2e46c152f2a954
Miguel angel Munoz fernandez <mmunoz-f@c1r4s3.42madrid.com>
2021-04-01 14:40:29 +0200

    01/04/2021 14:40:29 - better split it now accepts a sting of delimiters

--- ft_split.c
+++ ft_split.c
@@ -6,24 +6,24 @@
 /*   By: mmunoz-f <mmunoz-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/23 14:41:24 by mmunoz-f          #+#    #+#             */
-/*   Updated: 2021/03/13 18:07:15 by mmunoz-f         ###   ########.fr       */
+/*   Updated: 2021/04/01 14:39:59 by mmunoz-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdio.h>
 
-static size_t	word_len(char const *s, char c)
+static size_t	word_len(char const *s, const char *set)
 {
 	size_t	i;
 
 	i = 0;
-	while (s[i] && s[i] != c)
+	while (s[i] && !ft_strchr(set, s[i]))
 		i++;
 	return (i);
 }
 
-static int	ft_add_word(char ***f, char const *s, char c)
+static int	ft_add_word(char ***f, char const *s, const char *set)
 {
 	size_t	len;
 	char	**tmp;
@@ -42,20 +42,20 @@ static int	ft_add_word(char ***f, char const *s, char c)
 		(*f)[i] = tmp[i];
 		i++;
 	}
-	len = word_len(s, c);
+	len = word_len(s, set);
 	(*f)[i] = ft_substr(s, 0, len);
 	(*f)[i + 1] = 0;
 	free(tmp);
 	return (len);
 }
 
-char	**ft_split(char const *s, char c)
+char	**ft_split(char const *s, const char *set)
 {
 	char	**f;
 	size_t	i;
 	size_t	j;
 
-	if (!s)
+	if (!s || !set)
 		return (0);
 	i = 0;
 	f = (char **)malloc(sizeof(char *));
@@ -64,9 +64,9 @@ char	**ft_split(char const *s, char c)
 	*f = 0;
 	while (s[i])
 	{
-		if (s[i] != c)
+		if (!(ft_strchr(set, s[i])))
 		{
-			j = ft_add_word(&f, &s[i], c);
+			j = ft_add_word(&f, &s[i], set);
 			if (!j)
 				return (0);
 			i += j;
