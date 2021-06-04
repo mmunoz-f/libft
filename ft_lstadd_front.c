7a5bd92d88d61f157340dd829ffe84bb62c345b1
mmunoz-f <miguelmunozfdez@gmail.com>
2021-06-04 19:24:17 +0200

    04/06/2021 19:24:17 - better double linked list functions

--- ft_lstadd_front.c
+++ ft_lstadd_front.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstadd_front.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mmunoz-f <mmunoz-f@student.42.fr>          +#+  +:+       +#+        */
+/*   By: miguel <miguel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/25 16:30:21 by marvin            #+#    #+#             */
-/*   Updated: 2021/03/14 12:11:00 by mmunoz-f         ###   ########.fr       */
+/*   Updated: 2021/06/03 17:29:44 by miguel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,12 @@
 
 void	ft_lstadd_front(t_list **alst, t_list *new)
 {
+	if (!*alst)
+	{
+		*alst = new;
+		return ;
+	}
+	*alst = ft_lstfirst(*alst);
 	(*alst)->previous = new;
 	new->next = *alst;
 	*alst = new;
--- ft_lstclear.c
+++ ft_lstclear.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstclear.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mmunoz-f <mmunoz-f@student.42.fr>          +#+  +:+       +#+        */
+/*   By: miguel <miguel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/25 22:42:25 by marvin            #+#    #+#             */
-/*   Updated: 2021/03/10 12:20:24 by mmunoz-f         ###   ########.fr       */
+/*   Updated: 2021/06/03 17:30:28 by miguel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
 	t_list	*dnext;
 
+	*lst = ft_lstfirst(*lst);
 	while (*lst)
 	{
 		dnext = (*lst)->next;
